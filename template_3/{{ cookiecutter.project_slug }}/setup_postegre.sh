204aad3c779da77d87b77461ba8203b615cb4965
Kyytox <kytox.dev@protonmail.com>
2025-05-16 08:16:35 +0200

    move init db and rm files

--- template_3/{{ cookiecutter.project_slug }}/setup_postegre.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update the package list
-sudo apt update
-
-# Install PostgreSQL and its contrib package
-sudo apt install -y postgresql postgresql-contrib
-
-echo "PostgreSQL installation completed."
