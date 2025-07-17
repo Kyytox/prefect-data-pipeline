0a54f6d5ca4a188812e5be992302ace54d99fc4b
Kyytox <kytox.dev@protonmail.com>
2025-07-17 23:55:21 +0200

    clean pgms

--- init_project.py
+++ init_project.py
@@ -3,79 +3,6 @@ import os
 from cookiecutter.main import cookiecutter
 
 
-# def get_line_for_insert(content_pyproj):
-#     """
-#     Get the line number for inserting dependencies in pyproject.toml.
-
-#     Args:
-#         content_pyproj (str): Content of the pyproject.toml file.
-
-#     Returns:
-#         int: Line number for inserting dependencies.
-#     """
-#     # Find the dependencies section and first empty line after it
-#     start_index = content_pyproj.find("[tool.pixi.dependencies]")
-
-#     if start_index != -1:
-
-#         # Get substring starting from the section
-#         section_content = content_pyproj[start_index:]
-
-#         # Find first empty line using splitlines()
-#         lines = section_content.splitlines()
-
-#         for i, line in enumerate(lines):
-#             if line.strip() == "":
-#                 insert_position = start_index + sum(len(l) + 1 for l in lines[:i])
-#                 break
-#     else:
-#         print("Dependencies section not found in pyproject.toml")
-
-#     return insert_position
-
-
-# def update_pyproject_toml(content_pyproj, insert_position, select_libs):
-#     """
-#     Update the pyproject.toml file with the selected dependencies.
-#     """
-#     # Insert the selected dependencies at the insert position
-#     if insert_position:
-#         new_pyproject = (
-#             content_pyproj[:insert_position]
-#             + "\n".join([f"{lib}" for lib in select_libs])
-#             + "\n"
-#             + content_pyproj[insert_position:]
-#         )
-#         print("Dependencies inserted successfully.")
-#     else:
-#         print("Insert position not found, skipping insertion.")
-
-#     return new_pyproject
-
-
-# def get_version_libs(select_libs, json_libs):
-#     """
-#     Get the version of the librarys from the json file.
-
-#     Args:
-#         select_libs (list): List of selected libraries.
-#         json_libs (dict): Dictionary containing library dependencies.
-
-#     Returns:
-#         list: List of libraries with their versions.
-#     """
-#     new_libs = []
-
-#     for lib in select_libs:
-#         if lib in json_libs["dependencies"]:
-#             version = json_libs["dependencies"][lib]
-#             new_libs.append(version)
-#         else:
-#             print(f"Library {lib} not found in dependencies.json")
-
-#     return new_libs
-
-
 def check_response_template(num_template, dict_templates):
     """
     Check if the response is valid.
@@ -125,10 +52,19 @@ def get_selected_template(dict_templates, num_template):
 
 if __name__ == "__main__":
 
+    # Colors
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BLUE = "\033[0;34m"
+    YELLOW = "\033[1;33m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    NC = "\033[0m"  # No Color
+
     print("")
-    print("=" * 50)
-    print("  \033[1;33mWelcome to the Cookiecutter Template Creator !\033[00m")
-    print("=" * 50)
+    print(f"{BLUE}=" * 50)
+    print(f"  {YELLOW}Welcome to the Prefect Data Pipeline Creator !{NC}")
+    print(f"{BLUE}=" * 50)
 
     # get os path
     PATH = os.path.dirname(os.path.abspath(__file__))
@@ -145,31 +81,20 @@ if __name__ == "__main__":
     print("")
 
     for template in dict_templates:
-        print(
-            f"  \033[1;35m{template['id']}.\033[00m\033[1;33m{template['name']} \033[00m"
-        )
-        print(f"  -----------------")
-        print(f"  Description:")
-        print(f"  \033[1;36mDescription :\033[00m {template['description']}")
-        print(f"  \033[1;36mDependencies :\033[00m {template['dependencies']}")
-        print("  -----------------------------------------------------")
+        print(f"  {PURPLE}{template['id']}.{YELLOW}{template['name']} {NC}")
+        print(f"  {BLUE}-----------------{NC}")
+        print(f"  {CYAN}Description :{NC} {template['description']}")
+        print(f"  {CYAN}Dependencies :{NC} {template['dependencies']}")
+        print("")
         print("")
 
+    print("")
     # get response
     num_template = input("Enter the number of the template you want to use: ")
     print("")
 
     # check response
     num_template = check_response_template(num_template, dict_templates)
-
-    # # get the selected template where id = num_template
-    # selected_template = get_selected_template(dict_templates, num_template)
-
-    # # get the dependencies from the selected template
-    # select_libs = selected_template["dependencies"]
-
-    # # get version of the selected libraries
-    # select_libs = get_version_libs(select_libs, json_libs)
 
     # launch the cookiecutter command
     print("")
@@ -180,22 +105,8 @@ if __name__ == "__main__":
         output_dir=os.path.dirname(PATH),
     )
 
-    # Read pyproject.toml
-    # with open(os.path.join(NEW_PATH, "pyproject.toml"), "r") as f:
-    #     content_pyproj = f.read()
-
-    # # Get the line number for inserting dependencies
-    # insert_position = get_line_for_insert(content_pyproj)
-
-    # # Update pyproject.toml with the selected dependencies
-    # new_pyproject = update_pyproject_toml(content_pyproj, insert_position, select_libs)
-
-    # # Write the modified content back to pyproject.toml
-    # with open(os.path.join(NEW_PATH, "pyproject.toml"), "w") as f:
-    #     f.write(new_pyproject)
-
     print("")
-    print("=" * 50)
-    print(f"    \033[1;33mProject - {NEW_PATH.split('/')[-1]} - Created.\033[00m")
-    print(f"    \033[1;33m{NEW_PATH}\033[00m")
-    print("=" * 50)
+    print(f"{BLUE}=" * 50)
+    print(f"    {YELLOW}Project - {NEW_PATH.split('/')[-1]} - Created.{NC}")
+    print(f"    {YELLOW}{NEW_PATH}{NC}")
+    print(f"{BLUE}=" * 50)
